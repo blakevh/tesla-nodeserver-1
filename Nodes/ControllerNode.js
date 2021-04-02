37fdb549ce587f5403605dbf8fb499b9721c1d26
Blake Von Haden <6832052+blakevh@users.noreply.github.com>
2021-04-02 10:48:44 -0500

    Something is wrong with the ControllerNode

--- Nodes/ControllerNode.js
+++ Nodes/ControllerNode.js
@@ -82,7 +82,7 @@ module.exports = function(Polyglot) {
     }
 
     displayCredentialsError(err) {
-      if (err.message == "HTTP error 401") {
+      if (err.message && err.message == "HTTP error 401") {
         this.polyInterface.addNotice(
           'credsError',
           'Tesla account login failed'
